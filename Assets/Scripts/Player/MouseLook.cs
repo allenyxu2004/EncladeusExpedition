6aced3662848d2ea0441083cfbc2eb03a8f4bdba
kelvinx24 <kelvinjust4school@gmail.com>
2024-08-14 20:33:19 -0400

    Added last level

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
+public class MouseLook2 : MonoBehaviour
 {
     // Start is called before the first frame update
     Transform playerBody;
